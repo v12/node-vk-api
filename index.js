8a416692ddf9f07a6e0a479d485e56bc491c8a20
Nikita <dr.evil@krtn.eu>
2014-05-28 17:02:37 +0100

    Now it is possible to use both variants of parameters order (options, callback OR client_id, login, password, callback) when instantiating VK API

--- index.js
+++ index.js
@@ -19,88 +19,126 @@ function authorize(appId, login, pass, cb) {
         followAllRedirects: true
     });
 
-    var params = {
-        client_id: appId,
+    var default_params = {
+        client_id: 0,
+        login:     '',
+        pass:      '',
         scope:     [
-                       'notify',
-                       'friends',
-                       'photos',
-                       'audio',
-                       'video',
-                       'docs',
-                       'notes',
-                       'pages',
-                       'status',
-                       'offers',
-                       'questions',
-                       'wall',
-                       'groups',
-                       'messages',
-                       'notifications',
-                       'stats',
-                       'ads',
-                       'offline'
-                   ].join(','),
-        login:     login,
-        pass:      pass
+            'notify',
+            'friends',
+            'photos',
+            'audio',
+            'video',
+            'docs',
+            'notes',
+            'pages',
+            'status',
+            'offers',
+            'questions',
+            'wall',
+            'groups',
+            'messages',
+            'notifications',
+            'stats',
+            'ads',
+            'offline'
+        ]
     };
 
-    request({
-            url: url.format({
-                protocol: 'https',
-                host:     'oauth.vk.com',
-                pathname: 'authorize',
-                query:    {
-                    client_id:     params.client_id,
-                    scope:         params.scope,
-                    redirect_uri:  'https://oauth.vk.com/blank.html',
-                    display:       'mobile',
-                    v:             '5.21',
-                    response_type: 'token'
-                }
-            })
-        },
-        function (err, r, body) {
-            if (err)
-                return cb(err);
-
-            var nextQuery = getLoginFormData(body);
-
-            if (!('email' in nextQuery.form))
-                return cb(new Error('Unable to fetch login page'));
-
-            nextQuery.form.email = params.login;
-            nextQuery.form.pass = params.pass;
-
-            request({
-                    url:    nextQuery.url,
-                    method: 'POST',
-                    form:   nextQuery.form
-                },
-                function (err, r, body) {
-                    if (err)
-                        return cb(err);
-
-                    getAllowLink(body, function (err, link) {
-                        if (err)
-                            return cb(err);
-
-                        request({
-                            url:    link,
-                            method: 'POST'
-                        }, function (err, res) {
-                            var access_token = /access_token=([a-f0-9]+)/.exec(res.request.uri.hash);
-                            if (!access_token[1])
-                                return cb(new Error('Invalid access_token'));
-
-                            self.access_token = access_token[1];
-                            cb(null, self.access_token);
-
-                            self.emit('auth', self.access_token);
-                        });
-                    })
-                });
+    var params;
+    if (typeof appId === 'object') {
+        params = extend(default_params, appId);
+        cb = login;
+        pass = params.pass;
+        login = params.login;
+        appId = params.client_id;
+    }
+    else {
+        params = extend(default_params, {
+            client_id: appId,
+            login:     login,
+            pass:      pass
         });
+    }
+
+    // in case if only EventEmitter is used
+    if (typeof cb !== 'function')
+        cb = function () {};
+
+    var errorCallback = function (err) {
+        self.emit('error', err);
+        typeof cb !== 'function' && cb(err);
+    };
+
+    var e;
+    if (parseInt(appId) <= 0)
+        e = new Error('Invalid client_id');
+    else if (login.length <= 0)
+        e = new Error('Login was not defined');
+    else if (pass.length <= 0)
+        e = new Error('Password was not defined');
+
+    if(e) {
+        errorCallback(e);
+        return self;
+    }
+    else
+        request({
+                url: url.format({
+                    protocol: 'https',
+                    host:     'oauth.vk.com',
+                    pathname: 'authorize',
+                    query:    {
+                        client_id:     params.client_id,
+                        scope:         params.scope.join(','),
+                        redirect_uri:  'https://oauth.vk.com/blank.html',
+                        display:       'mobile',
+                        v:             '5.21',
+                        response_type: 'token'
+                    }
+                })
+            },
+            function (err, r, body) {
+                if (err)
+                    return cb(err);
+
+                var nextQuery = getLoginFormData(body);
+
+                if (!('email' in nextQuery.form))
+                    return errorCallback(new Error('Unable to fetch login page'));
+
+                nextQuery.form.email = params.login;
+                nextQuery.form.pass = params.pass;
+
+                request({
+                        url:    nextQuery.url,
+                        method: 'POST',
+                        form:   nextQuery.form
+                    },
+                    function (err, r, body) {
+                        if (err)
+                            return errorCallback(err);
+
+                        getAllowLink(body, function (err, link) {
+                            if (err)
+                                return errorCallback(err);
+
+                            request({
+                                url:    link,
+                                method: 'POST'
+                            }, function (err, res) {
+                                var access_token = /access_token=([a-f0-9]+)/.exec(res.request.uri.hash);
+                                if (!access_token[1])
+                                    return errorCallback(new Error('Invalid access_token'));
+
+                                self.access_token = access_token[1];
+                                cb(null, self.access_token);
+
+                                self.emit('auth', self.access_token);
+                            });
+                        })
+                    });
+            });
 
     function getLoginFormData(html) {
         var $ = cheerio.load(html),
@@ -153,17 +191,17 @@ function authorize(appId, login, pass, cb) {
                     })
             })
         }, function (err, r, json) {
-            if(err)
+            if (err)
                 return cb(err);
 
             var response = JSON.parse(json);
-            if('error' in response) {
+            if ('error' in response) {
                 var e = new Error(response.error.error_msg);
                 e.code = response.error.error_code;
                 return cb(e);
             }
 
-            if((!'response' in response))
+            if ((!'response' in response))
                 return cb(new Error('No `response` field in API response'));
 
             cb(null, response.response);
